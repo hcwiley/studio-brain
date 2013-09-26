13ac83b80a5fff1ef948737d06086868b6b5ef24
H. Cole Wiley <hcwiley@gmail.com>
2013-09-26 14:27:49 -0500

    just got this back up and running with the stepper

--- doorLock/doorLock.ino
+++ doorLock/doorLock.ino
@@ -1,13 +1,14 @@
 
 #include <Stepper.h>
 
-const int steps = 200;  // change this to fit the number of steps per revolution
+const int steps = 2000;  // change this to fit the number of steps per revolution
                                      // for your motor
 
 Stepper myStepper(steps, 12,11, 13,3);
 
 int powIn;                                
 int total;
+char dir;
 void setup()
 {
   Serial.begin(9600);
@@ -20,18 +21,24 @@ void setup()
 void loop()
 {
   while(!Serial.available()){}
-  char dir = Serial.read();
-  if(dir == 'l'){
+  char readIn = Serial.read();
+  if(readIn == 'l'){
+    dir = readIn;
     for(int i = 0; i < 150; i++){
       myStepper.step(1);
       delay(10);
     }
+    myStepper.step(-1);
   }
-  else if( dir == 'r'){
+  else if( readIn == 'r'){
     for(int i = 0; i < 150; i++){
       myStepper.step(-1);
       delay(10);
     }
+    myStepper.step(-1);
+  }
+  else {
+    myStepper.step(-1);
   }
 //  delay(10);
 }
