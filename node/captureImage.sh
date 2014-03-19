b945ed70ec14da8447cb71a6963c670da538aa7b
H. Cole Wiley <hcwiley@gmail.com>
2014-03-19 18:35:36 -0500

    updated captureImage

--- node/captureImage.sh
+++ node/captureImage.sh
@@ -1,26 +1,26 @@
-sysctl vm.overcommit_memory=1
+#sysctl vm.overcommit_memory=1
+SOURCE=""
 if [ -c /dev/video0 ]; then
-  echo "use video0"
-  streamer -c /dev/video0 -o public/img/foobar.jpeg &
+  SOURCE="/dev/video0"
 else 
   if [ -c /dev/video1 ]; then
-    echo "use video1"
-    streamer -c /dev/video1 -o public/img/foobar.jpeg &
+    SOURCE="/dev/video1"
   fi
 fi
+streamer -c $SOURCE -o public/img/foobar.jpeg &
 sleep 0.5
-sysctl vm.overcommit_memory=0
+#sysctl vm.overcommit_memory=0
 PID=$(pidof streamer)
-echo $PID
+#echo $PID
 # reset the device if PID
 if [ $PID ]; then
   killall streamer
-  FULLSTR=$(lsusb | grep cam)
-  USBBUS='/dev/bus/usb/'
-  USBBUS=$USBBUS$(echo $FULLSTR  | cut -d" " -f 2)'/'
-  USBBUS=$USBBUS$(echo $FULLSTR  | cut -d" " -f 4)
-  USBBUS=$(echo $USBBUS  | cut -d":" -f 1)
-  #echo $USBBUS
-  ./resetUSB $USBBUS
+  #FULLSTR=$(lsusb | grep cam)
+  #USBBUS='/dev/bus/usb/'
+  #USBBUS=$USBBUS$(echo $FULLSTR  | cut -d" " -f 2)'/'
+  #USBBUS=$USBBUS$(echo $FULLSTR  | cut -d" " -f 4)
+  #USBBUS=$(echo $USBBUS  | cut -d":" -f 1)
+  #./resetUSB $USBBUS
 fi
+
 
